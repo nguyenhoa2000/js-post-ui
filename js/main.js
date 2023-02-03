bcee89fce83e27ff3bf478e5aadddea99544e964
Hoa Nguyen <hoa678815@gmail.com>
2023-02-03 11:59:23 +0700

    setup api file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log('test')
+import postApi from './api/postApi'
+
+const main = async () => {
+  const queryParams = {
+    _page: 1,
+    _limit: 5,
+  }
+  const id = 'lea2aa9l7x3a5tg'
+  const data1 = {
+    title: 'test3',
+    author: 'it me',
+    description: 'test description',
+    imageUrl: 'https://picsum.photos/id/559/1368/400',
+  }
+
+  try {
+    const response = await postApi.addData(data1)
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
