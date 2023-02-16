00a5a155ec5c1708c91346a008599ca1f1aaec91
Hoa Nguyen <hoa678815@gmail.com>
2023-02-17 04:05:51 +0700

    handle validate form with yup

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -1,4 +1,28 @@
-import { setFieldValue, setBackgroudImg } from './common'
+import { setFieldValue, setBackgroudImg, setTextContent } from './common'
+import * as yup from 'yup'
+
+const getPostSchema = () => {
+  return yup.object().shape({
+    title: yup
+      .string()
+      .required('please enter title')
+      .test(
+        '',
+        'Please enter at least 3 words',
+        (value) => value.split(' ').filter((item) => item.length > 3).length > 2
+      ),
+    author: yup
+      .string()
+      .required('please enter author')
+      .test(
+        '',
+        'Please enter at least 10 characters',
+        (value) =>
+          value.split(' ').reduce((accumulator, item) => accumulator + item, '').length > 10
+      ),
+    description: yup.string(),
+  })
+}
 
 const setFormValue = (form, formValue) => {
   setFieldValue(form, '[name="title"]', formValue?.title)
@@ -23,6 +47,36 @@ const getFormValues = (form) => {
   return formValues
 }
 
+const setFieldError = (form, name, error) => {
+  const element = form.querySelector(`[name="${name}"]`)
+  if (element) {
+    element.setCustomValidity(error)
+    setTextContent(element.parentElement, '.invalid-feedback', error)
+  }
+}
+
+const validatePostForm = async (form, formValues) => {
+  try {
+    ;['title', 'author'].forEach((name) => setFieldError(form, name, ''))
+    const schema = getPostSchema()
+    await schema.validate(formValues, { abortEarly: false })
+  } catch (error) {
+    console.log(error.inner)
+
+    const errorLog = {}
+    for (const validationError of error.inner) {
+      const name = validationError.path
+      if (errorLog[name]) continue
+      setFieldError(form, name, validationError.message)
+      errorLog[name] = true
+    }
+  }
+
+  const ivalid = form.checkValidity()
+  if (!ivalid) form.classList.add('was-validated')
+  return ivalid
+}
+
 export const initPostForm = ({ formId, defaultValue, onSubmit }) => {
   const form = document.getElementById(formId)
   setFormValue(form, defaultValue)
@@ -31,7 +85,7 @@ export const initPostForm = ({ formId, defaultValue, onSubmit }) => {
     e.preventDefault()
     const formValues = getFormValues(form)
     console.log(formValues)
+
+    if (!validatePostForm(form, formValues)) return
   })
 }
-
-
